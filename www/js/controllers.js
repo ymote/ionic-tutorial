b61ed4a72e4ec29c2acdd585f189c62201d8ecda
live4code <live4code@gmail.com>
2015-03-22 20:06:08 +0000

    add ionic range

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,6 +43,14 @@ angular.module('movie.controllers', [])
     }); 
   };
 
+  $scope.$watch('deduction', function(){
+    var high = 10;
+    var low = high - parseInt($scope.deduction);
+    currentPage = 0;
+    MovieService.filterByRating(low, high, currentPage, limit).then(function(movies){
+      $scope.movies = movies;
+    });
+  });
 
   //shows the usage of $ionicPopover, see ionic documentation for detail explanation.
   //the name for the popover view is 'templates/popover', it is the templateUrl for the $ionicPopover directive.
